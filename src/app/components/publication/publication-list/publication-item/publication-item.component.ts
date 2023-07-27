d420b2b4ad7d994a9454e18a6a034db6af2d8478
yassineferjani <yassineferjani9@gmail.com>
2023-07-27 23:01:41 +0100

    Added all publication component using ngrx

--- src/app/components/publication/publication-list/publication-item/publication-item.component.ts
+++ src/app/components/publication/publication-list/publication-item/publication-item.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Publication } from 'src/app/models/Publication.model';
+import { deletePublication } from 'src/app/ngrx/publication/publication.actions';
 
 @Component({
   selector: 'app-publication-item',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./publication-item.component.css']
 })
 export class PublicationItemComponent {
+  @Input() publication?:Publication
+
+  constructor(private store:Store<any>, private router:Router){}
+
+  onDelete(publication :Publication){
+    this.store.dispatch(deletePublication({publication}))
+  }
+  onEdit(publication:Publication){
+    this.router.navigateByUrl("/publication/edit/"+publication.id)
+  }
 
 }
--- src/app/components/publication/publication-navbar/publication-navbar.component.ts
+++ src/app/components/publication/publication-navbar/publication-navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { getPublications } from 'src/app/ngrx/publication/publication.actions';
 
 @Component({
   selector: 'app-publication-navbar',
   templateUrl: './publication-navbar.component.html',
   styleUrls: ['./publication-navbar.component.css']
 })
-export class PublicationNavbarComponent {
+export class PublicationNavbarComponent implements OnInit{
+  constructor(private router:Router, private store:Store<any>){}
+  ngOnInit(): void {
+  }
+
+
+  onNewPub(){
+    this.router.navigateByUrl("/publication/new")
+  }
+  onGetAllPubs(){
+    this.store.dispatch(getPublications())
+  }
 
 }
