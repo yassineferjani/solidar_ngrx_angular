629be5e31a36aa5e9b15c7329f893cdb214d010a
yassineferjani <yassineferjani9@gmail.com>
2023-07-30 11:23:32 +0100

    Added Publication crud with ngrx

--- src/app/components/publication/publication-edit/publication-edit.component.ts
+++ src/app/components/publication/publication-edit/publication-edit.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { editPublication, updatePublication } from 'src/app/ngrx/publication/publication.actions';
+import { PublicationState } from 'src/app/store/publication/Publication.store';
+import { PublicationStateEnum } from 'src/app/store/publication/PublicationStore.enum';
 
 @Component({
   selector: 'app-publication-edit',
   templateUrl: './publication-edit.component.html',
   styleUrls: ['./publication-edit.component.css']
 })
-export class PublicationEditComponent {
+export class PublicationEditComponent implements OnInit {
+
+  pubFormGroup?:FormGroup;
+  state?:PublicationState;
+  submitted:Boolean=false;
+  readonly PublicationStateEnum = PublicationStateEnum;
+  publicationId:number;
+
+  constructor(private store:Store<any>,private fb:FormBuilder, private router:Router,
+    private activatedRoute: ActivatedRoute){
+      this.publicationId = activatedRoute.snapshot.params['id'];
+    }
+
+  ngOnInit(): void {
+    this.store.dispatch(editPublication({id : this.publicationId}))
+    this.store.subscribe((state)=>{
+      this.state = state.catalogPublication;
+      this.pubFormGroup= this.fb.group({
+        id:[this.state?.currentPublication?.id],
+        author:[this.state?.currentPublication?.author,Validators.required],
+        title:[this.state?.currentPublication?.title,Validators.required],
+        intro:[this.state?.currentPublication?.intro,Validators.required],
+        date:[this.state?.currentPublication?.date,Validators.required],
+        images:[this.state?.currentPublication?.images,Validators.required],
+        path:[this.state?.currentPublication?.path,Validators.required],
+
+      });
+    })
+
+  }
+
+  updated(){
+    this.router.navigateByUrl("/publication");
+  }
+
+  onUpdatePub(){
+    this.submitted=true;
+    if(this.pubFormGroup?.invalid)
+      return;
+    this.store.dispatch(updatePublication({publication : this.pubFormGroup?.value}))
+
+  }
+
 
 }
--- src/app/components/publication/publication-new/publication-new.component.ts
+++ src/app/components/publication/publication-new/publication-new.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { createPublication, newPublication } from 'src/app/ngrx/publication/publication.actions';
+import { PublicationState } from 'src/app/store/publication/Publication.store';
+import { PublicationStateEnum } from 'src/app/store/publication/PublicationStore.enum';
 
 @Component({
   selector: 'app-publication-new',
   templateUrl: './publication-new.component.html',
   styleUrls: ['./publication-new.component.css']
 })
-export class PublicationNewComponent {
+export class PublicationNewComponent implements OnInit {
+  pubFormGroup?:FormGroup;
+  state?:PublicationState;
+  readonly PublicationStateEnum = PublicationStateEnum;
+  submitted:Boolean = false;
+
+  constructor(private fb:FormBuilder, private store:Store<any>){}
+
+  ngOnInit(): void {
+    this.store.dispatch(newPublication())
+    this.store.subscribe((state)=>{
+        this.state = state.catalogPublication;
+        if(this.state?.dataState == PublicationStateEnum.NEW){
+          this.pubFormGroup = this.fb.group({
+            author:["",Validators.required],
+            title:["",Validators.required],
+            intro:["",Validators.required],
+            images:["",Validators.required],
+            date:["",Validators.required],
+            path:["",Validators.required],
+          });
+        }
+
+    })
+  }
+
+  newPub(){
+    this.store.dispatch(newPublication())
+  }
+
+  onSavePub(){
+    this.submitted=true;
+    if (this.pubFormGroup?.invalid)
+      return
+    this.store.dispatch(createPublication({publication : this.pubFormGroup?.value}))
+  }
 
 }
